7c487d366194f5de21acbf4d2d87961f51149751
robynp108 <robynp108@gmail.com>
2020-03-24 20:54:30 -0500

    final code clean up, html validation and comments

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
+//variables to access elements
 var timerEl = document.getElementById("timer");
 var startEl = document.getElementById("start");
+var startButtonEl = document.getElementById("start-button");
 var questionsEl = document.getElementById("questionCard");
 var questionEl = document.getElementById("questions");
 var answer1El = document.getElementById("answer1");
@@ -10,87 +12,86 @@ var finishEl = document.getElementById("finish");
 var finalScoreEl = document.getElementById("final-score");
 
 var i = 0;
-var timeLeft = 10;
+var timeLeft = 15;
 
-startEl.addEventListener("click", function() {
-    quizTimer();
-    startEl.setAttribute("style", "display: none");
-    askQuestions();
+//event handler for start button
+startButtonEl.addEventListener("click", function () {
+  quizTimer();
+  startEl.setAttribute("style", "display: none");
+  askQuestions();
 })
 
+//timer function
 function quizTimer() {
-    
-    var timeInterval = setInterval(function() {
-      timerEl.textContent = timeLeft + " seconds remaining";
-     
-      if (questionArray[i] == undefined) {
-        questionsEl.setAttribute("style", "display: none");
-        finishEl.setAttribute("style", "display: auto");
-        clearInterval(timeInterval);
-      } else if (timeLeft === 0) {
-        questionsEl.setAttribute("style", "display: none");
-        finishEl.setAttribute("style", "display: auto");
-        clearInterval(timeInterval);
-      } else {timeLeft--;}
-  
-    }, 1000);
-  }
 
-  var questionArray = [
-	{
-		title: "Which of these is not a programming language?",
-		choices: ["HTML", "CSS", "jQuery", "JavaScript"],
-		answer: "jQuery"
-	},
-	{
-		title: "What will console.log(\"2\" + \"5\") return?",
-		choices: ["7", "10", "null", "25"],
-		answer: "25"
-  },
-  {
-		title: "What does the .push() method do?",
-    choices: ["push element to end of line", "push element to end of page", 
-    "append data to local storage", "append item to an array"],
-		answer: "append item to an array"
-  },
-  {
-		title: "Which of these operators means 'or'?",
-		choices: ["^^", "||", "!!", "&&"],
-		answer: "||"
-	}
-]
+  var timeInterval = setInterval(function () {
+    timerEl.textContent = timeLeft + " seconds remaining";
 
-function askQuestions () {
-    questionsEl.setAttribute("style", "display: auto");
-    displayQuestion(questionArray[i]);
+    if (questionArray[i] == undefined) {
+      questionsEl.setAttribute("style", "display: none");
+      finishEl.setAttribute("style", "display: auto");
+      clearInterval(timeInterval);
+    } else if (timeLeft === 0) {
+      questionsEl.setAttribute("style", "display: none");
+      finishEl.setAttribute("style", "display: auto");
+      clearInterval(timeInterval);
+    } else { timeLeft--; }
+
+  }, 1000);
 }
 
-function displayQuestion (question) {
-    questionEl.textContent = question["title"];
-    answer1El.textContent = question["choices"][0];
-    answer2El.textContent = question["choices"][1];
-    answer3El.textContent = question["choices"][2];
-    answer4El.textContent = question["choices"][3];
+//question array
+var questionArray = [
+  {
+    title: "Which of these is not a programming language?",
+    choices: ["HTML", "CSS", "jQuery", "JavaScript"],
+    answer: "jQuery"
+  },
+  {
+    title: "What will console.log(\"2\" + \"5\") return?",
+    choices: ["7", "10", "null", "25"],
+    answer: "25"
+  },
+  {
+    title: "What does the .push() method do?",
+    choices: ["push element to end of line", "push element to end of page",
+      "append data to local storage", "append item to an array"],
+    answer: "append item to an array"
+  },
+  {
+    title: "Which of these operators means 'or'?",
+    choices: ["^^", "||", "!!", "&&"],
+    answer: "||"
+  }
+]
+
+//functions to start display of questions
+function askQuestions() {
+  questionsEl.setAttribute("style", "display: auto");
+  displayQuestion(questionArray[i]);
+}
+
+function displayQuestion(question) {
+  questionEl.textContent = question["title"];
+  answer1El.textContent = question["choices"][0];
+  answer2El.textContent = question["choices"][1];
+  answer3El.textContent = question["choices"][2];
+  answer4El.textContent = question["choices"][3];
 }
 
 var score = 0
 
-function answerChoice (userChoice) {
-//console.log(userChoice);
-  //decide if correct
-//store if correct
-//time off timer if incorrect
+//functions to validate user answer choices, move to next question and decide if finished
+function answerChoice(userChoice) {
   var question = questionArray[i];
-  //console.log(question["choices"][userChoice]);
-  // console.log(question["answer"]);
+
   if (question["choices"][userChoice] == question["answer"]) {
     score++;
     console.log(score);
   } else {
     timeLeft = timeLeft - 3;
   }
-//move to next question- i++
-//display next question
+
   i++;
   if (questionArray[i] == undefined) {
     questionsEl.setAttribute("style", "display: none");
@@ -99,41 +100,40 @@ function answerChoice (userChoice) {
   } else {
     displayQuestion(questionArray[i]);
   }
-//decide if done
-  
 }
 
-answer1El.addEventListener("click", function() {
+//event handlers for answer buttons
+answer1El.addEventListener("click", function () {
   answerChoice(0);
 });
-answer2El.addEventListener("click", function() {
+answer2El.addEventListener("click", function () {
   answerChoice(1);
 });
-answer3El.addEventListener("click", function() {
+answer3El.addEventListener("click", function () {
   answerChoice(2);
 });
-answer4El.addEventListener("click", function() {
+answer4El.addEventListener("click", function () {
   answerChoice(3);
 });
 
-//event handler for submit button
+//event handler for submit button with storage of scores
 var submitButton = document.getElementById("button-addon2");
 
-submitButton.addEventListener("click", function() {
-    //access value of input field
-    var initialsEl = document.getElementById("user-initials");
-    
-    var highScoreEntry = initialsEl.value + " - " + score;
-    //console.log(highScoreEntry);
-    var highScores;
+submitButton.addEventListener("click", function () {
 
-    if (localStorage.getItem("highScores") === null) {
-      highScores = [];
-    } else {
-      highScores = JSON.parse(localStorage.getItem("highScores"));
-    }
-    
-    highScores.push(highScoreEntry);
+  var initialsEl = document.getElementById("user-initials");
 
-    localStorage.setItem("highScores", JSON.stringify(highScores));
+  var highScoreEntry = initialsEl.value + " - " + score;
+
+  var highScores;
+
+  if (localStorage.getItem("highScores") === null) {
+    highScores = [];
+  } else {
+    highScores = JSON.parse(localStorage.getItem("highScores"));
+  }
+
+  highScores.push(highScoreEntry);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 })
--- score.js
+++ score.js
@@ -1,24 +1,7 @@
-// //event handler for submit button
-// var submitButton = document.getElementById("button-addon2");
-
-// submitButton.addEventListener("click", function() {
-//     //access value of input field
-//     var scoreListEl = document.getElementById("score-list");
-//     var initialsEl = document.getElementById("user-initials");
-    
-//     var userInitals = initialsEl.value;
-//     var currentScore = document.createElement("li");
-//     currentScore.textContent = userInitals + "-" + score;
-
-//     console.log(currentScore);
-
-//     scoreListEl.appendChild(currentScore);
-    
-// })
-
+//function to retrieve scores from storage and append new score to list
 var highScores = JSON.parse(localStorage.getItem("highScores"));
 
-highScores.forEach(function(highScoreEntry) {
+highScores.forEach(function (highScoreEntry) {
     var scoreListEl = document.getElementById("score-list");
     var currentScore = document.createElement("li");
     currentScore.textContent = highScoreEntry;
